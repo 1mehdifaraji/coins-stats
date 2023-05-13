fc6f3df5bb19686eca7deeb186d42e52a0141b10
mehdi faraji <mahdifarajideveloper@gmail.com>
2023-05-14 02:32:23 +0330

    added react query configuration

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import Home from "pages/Home";
+
+import "index.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
+const queryClient = new QueryClient();
+
 root.render(
-  <React.StrictMode>
-    <div />
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <Home />
+  </QueryClientProvider>
 );
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface HomeProps {}
+
+const Home: FC<HomeProps> = () => {
+  return <div>Homepage</div>;
+};
+
+export default Home;
