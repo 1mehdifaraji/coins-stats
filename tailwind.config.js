843a56700927c3d720ab7a08e3f8ba4b416a69d3
mehdi faraji <mahdifarajideveloper@gmail.com>
2023-05-14 02:23:55 +0330

    changed tailwindcss config , edited index.css styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    fontSize: {
+      xs: "0.6rem",
+      sm: "0.8rem",
+    },
+    extend: {
+      animation: {
+        fadeIn: "fadeIn 0.6s",
+        fadeInQuick: "fadeIn 0.4s",
+        fadeInSlow: "fadeIn 1.3s",
+        wiggle: "wiggle 300ms ease-in-out",
+      },
+      keyframes: () => ({
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+        wiggle: {
+          "0%, 100%": { transform: "none" },
+          "50%": { transform: "scale(0.97)" },
+        },
+      }),
+      colors: {
+        dark: "#1a1d21",
+        darkLight: "#34373c",
+      },
+    },
   },
   plugins: [],
 };
