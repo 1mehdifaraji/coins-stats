6bcf40c0cd6ffbdcebbb225933156b992a9e50c9
mehdi faraji <mahdifarajideveloper@gmail.com>
2023-05-16 10:46:28 +0330

    fixed search and fav coins tests issues

--- cypress/e2e/fav.cy.ts
+++ cypress/e2e/fav.cy.ts
@@ -1,19 +1,19 @@
 describe("Favourite coins", () => {
   it("Toggle favourite coins", () => {
     cy.visit("/");
-    cy.wait(15000);
+    cy.wait(10000);
 
     cy.get(
-      ":nth-child(2) > :nth-child(1) > .w-60 > :nth-child(1) > :nth-child(1)"
+      ":nth-child(3) > :nth-child(1) > .w-60 > :nth-child(1) > :nth-child(1)"
     ).click();
     cy.get('[data-testid="switch"]').click();
     cy.wait(300);
     cy.get(
-      ":nth-child(3) > :nth-child(1) > .w-60 > .space-x-2 > :nth-child(2)"
+      ".py-3.flex > :nth-child(1) > .w-60 > .space-x-2 > :nth-child(2)"
     ).should("contain.text", "Tether");
     cy.get('[data-testid="switch"]').click();
     cy.get(
-      ":nth-child(6) > :nth-child(1) > .w-60 > .space-x-2 > :nth-child(2)"
+      ":nth-child(1) > :nth-child(1) > .w-60 > .space-x-2 > :nth-child(2)"
     ).should("contain.text", "Bitcoin");
   });
 });
